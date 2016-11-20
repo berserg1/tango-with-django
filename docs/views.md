9dbb5ad5cb39cf4c01d074f11d0187f980a58da6
Sergey Berezhnoy <sergejs.bereznojs@gmail.com>
2016-11-20 18:46:56 +0200

    Finish chapter 3

--- docs/views.md
+++
@@ -1,4 +0,0 @@
-# Django Views 
-
-Django views is a set of functions used to handle interactions between a client and a database.  
-Each view takes a request and returns some response (HttpResponse).
--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,10 @@ from django.http import HttpResponse
 
 
 def index(reqest):
-    return HttpResponse("Rango says hey there partner!")
+    html = "<html><body><p>Rango says hey there partner!</p><p><a href='/rango/about/'>About</a></p></body></html>"
+    return HttpResponse(html)
+
+
+def about(reqest):
+    html = "<html><body><p>Rango says here is the about page</p><p><a href='/rango/'>Index</a></p></body></html>"
+    return HttpResponse(html)
