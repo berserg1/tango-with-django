5c105fbf8267ace2c00de536c3fb4d43f64661a0
Sergey <sergejs.bereznojs@gmail.com>
2016-12-06 16:24:49 +0200

    Almost finished Models chapter, added population script

--- rango/models.py
+++ rango/models.py
@@ -2,10 +2,16 @@ from django.db import models
 
 # Create your models here.
 
+
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
 
-    def __str__(self): # For Python 2, use __unicode__ too
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):  # For Python 2, use __unicode__ too
         return self.name
 
 
--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def index(reqest):
     # Construct a dictionary to pass to the template engine as its context
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
@@ -10,6 +11,7 @@ def index(reqest):
     # We make use of the shortcut function to make our lives easier
     # Note that the first parameter is the template we wish to use.
     return render(reqest, 'rango/index.html', context=context_dict)
+
 
 def about(reqest):
     context_dict = {'textblock': "This tutorial has been put together by Sergey."}
