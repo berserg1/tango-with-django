90c749378fdb7d92528a5fc087a193a7d053f932
Sergey <sergejs.bereznojs@gmail.com>
2016-12-07 17:37:43 +0200

    Chapter 6: index view and admin

--- rango/admin.py
+++ rango/admin.py
@@ -2,7 +2,23 @@ from django.contrib import admin
 from rango.models import Category, Page
 
 
-admin.site.register(Category)
-admin.site.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'url']
+    search_fields = ['title']
+    list_filter = ['views']
 
-# Register your models here.
+
+class PageInline(admin.TabularInline):
+    model = Page
+    extra = 3
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'views', 'likes']
+    list_filter = ['views', 'likes']
+
+    inlines = [PageInline]
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
